b0dccf95b1790ca5131d02519a86f4a3aaf97263
ScrapNoodles <144817703+ScrapNoodles@users.noreply.github.com>
2024-11-13 19:34:56 -0800

    Duplicate persistent object fix
    problem of duplicate persistent objects fixed.. for now...

---
+++ Assets/Scripts/Score System/Persistent Manager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentManager : MonoBehaviour
+{
+    public GameObject[] objects;
+
+    /*
+     * Fixes the issue of multiple persistent value instances appearing and fucking up the score
+     * makes an array and puts every persistent value object into it
+     * anything that isnt at the index of 0 is destroyed
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objects = GameObject.FindGameObjectsWithTag("Persistent Value");
+
+        foreach (GameObject i in objects)
+        {
+            if (objects.Length >= 2)
+            {
+                Destroy(objects[objects.Length - 1]);
+            }//end if
+
+        }//end foreach
+
+    }//end start
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }//end update
+}//end class
--- Assets/Scripts/Score System/PersistentValue.cs
+++ Assets/Scripts/Score System/PersistentValue.cs
@@ -11,46 +11,93 @@ public class PersistentValue : MonoBehaviour
     public float score;
     public ScoreValue ScoreValue;
 
+    //values for debugging.log ONCE
+    public bool scoreValueTxt;
+    public bool scoreTxt;
+    public bool gameObjectTxt;
+
     // Start is called before the first frame update
     void Start()
     {
+
         DontDestroyOnLoad(this.gameObject); //should be the code that doesnt kill the object across scenes
-        try
-        {
-            //finds the nearest object with score tag (should be just the score value)
-            scoreValue = GameObject.FindGameObjectWithTag("Score"); 
-            Debug.Log("found GameObject");
-        }//end try
-        catch
-        {
-            Debug.Log("GameObject not found");
-        }//end catch
-        
-    }
+
+        scoreValueTxt = false;
+        scoreTxt = false;
+        gameObjectTxt = false;
+
+    }//end start
 
     // Update is called once per frame
     void Update()
     {
+        try
+        {
+            //finds the nearest object with score tag (should be just the score value)
+            scoreValue = GameObject.FindGameObjectWithTag("Score");
+
+            if (gameObjectTxt!) //plays once...
+            {
+                Debug.Log("found GameObject");
+                gameObjectTxt = true;
+            }//end if
+            
+        }//end try
+        catch
+        {
+            if (gameObjectTxt!) //plays once...
+            {
+                Debug.Log("GameObject not found");
+                gameObjectTxt = true;
+            }//end if 
+
+        }//end catch
+
         //if scorevalue object = true (if there IS a scorevalue object present)
         if (scoreValue)
         {
             //pulls ScoreValue script from gameobject
             ScoreValue = scoreValue.GetComponent<ScoreValue>();
-            Debug.Log("found scorevalue code");
+
+            if (scoreValueTxt!) //plays once...
+            {
+                Debug.Log("found scorevalue code");
+                scoreValueTxt = true;
+            }//end if
+            
         }//end if
+
         else
         {
-            Debug.Log("scorevalue code not found");
+            if (scoreValueTxt!) //plays once...
+            {
+                Debug.Log("scorevalue code not found");
+                scoreValueTxt = true;
+            }//end if
+
         }//end else
 
         try
         {
             //turns scorevalue's score value into persistent value's score
             score = ScoreValue.score;
-        }//end try
+
+            if (scoreTxt!) //plays once...
+            {
+                Debug.Log("score found");
+                scoreTxt = true;
+            }//end if 
+
+            }//end try
+
         catch
         {
-            Debug.Log("score not found");
+            if (scoreTxt!) //plays once...
+            {
+                Debug.Log("score not found");
+                scoreTxt = true;
+            }//end if
+            
         }//end catch
 
     }//end update
