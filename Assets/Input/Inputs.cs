5a2443beb3cb96426766ed4def36f6e61e3113d4
Shallowcat <joshR364@hotmail.com>
2024-11-15 14:15:37 -0500

    test

--- Assets/Input/Inputs.cs
+++ Assets/Input/Inputs.cs
@@ -407,9 +407,6 @@ public partial class @Inputs: IInputActionCollection2, IDisposable
             @GasBreak.started -= instance.OnGasBreak;
             @GasBreak.performed -= instance.OnGasBreak;
             @GasBreak.canceled -= instance.OnGasBreak;
-            @ControlerCam.started -= instance.OnControlerCam;
-            @ControlerCam.performed -= instance.OnControlerCam;
-            @ControlerCam.canceled -= instance.OnControlerCam;
         }
 
         public void RemoveCallbacks(IInputActions instance)
@@ -434,6 +431,5 @@ public partial class @Inputs: IInputActionCollection2, IDisposable
         void OnHmdPosition(InputAction.CallbackContext context);
         void OnSteer(InputAction.CallbackContext context);
         void OnGasBreak(InputAction.CallbackContext context);
-        void OnControlerCam(InputAction.CallbackContext context);
     }
 }
