ffd1045dcccb77d0b3316723a0f0315a8123284f
Shallowcat <joshR364@hotmail.com>
2024-11-16 18:42:41 -0500

    tag fix

--- Assets/Scripts/CarCollision.cs
+++ Assets/Scripts/CarCollision.cs
@@ -6,8 +6,9 @@ public class CarCollision : MonoBehaviour
 {
     public float score = 0;
 
-    public void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
+        
         if (collision.gameObject.tag == "Traffic")
         {
                 Debug.Log("Collision");
